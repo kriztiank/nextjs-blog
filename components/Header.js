7829eda84ce174f508ff2b8f52589dfc8fb0ef04
kriztiank <kriztiank@gmail.com>
2021-11-25 09:43:10 +0100

    Header & Tailwind Styling

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <header className='bg-blue-600 text-gray-100 shadow'>
+      <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+        <Link href='/'>
+          <a className='flex title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
+            <Image src='/images/logo.png' width={40} height={40} alt='logo' />
+            <span className='ml-3 text-xl'>DevSpace</span>
+          </a>
+        </Link>
+        <nav className='flex flex-wrap items-center justify-end text-base md:ml-auto'>
+          <Link href='/blog'>
+            <a className='mx-5 cursor-pointer hover:text-indigo-300'>
+              Blog
+            </a>
+          </Link>
+          <Link href='/about'>
+            <a className='mx-5 cursor-pointer hover:text-indigo-300'>
+              About
+            </a>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
