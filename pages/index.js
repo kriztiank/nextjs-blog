09d4df226107e29ed3c9b05b7d60f3e711f9ae66
kriztiank <kriztiank@gmail.com>
2021-11-28 12:00:42 +0100

    Sort Posts By Date

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import matter from 'gray-matter'
 import Layout from '../components/Layout'
 import Post from '../components/Post'
-// import { sortByDate } from '../utils'
+import { sortByDate } from '../utils'
 
 export default function HomePage({ posts }) {
   console.log(posts)
@@ -48,7 +48,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   }
 }
