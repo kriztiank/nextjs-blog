6d2cbadc281256c0a80299c05c2f7ae5d40a070d
kriztiank <kriztiank@gmail.com>
2021-11-29 10:54:45 +0100

    Generate Static Paths & Single Post Data

---
+++ pages/blog/[slug].js
@@ -0,0 +1,48 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+// import marked from 'marked'
+// import Link from 'next/link'
+// import Layout from '@/components/Layout'
+// import CategoryLabel from '@/components/CategoryLabel'
+
+export default function PostPage({
+  frontmatter: { title, category, date, cover_image, author, author_image },
+  content,
+  slug,
+}) {
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(
+    path.join('posts', slug + '.md'),
+    'utf-8'
+  )
+
+  const { data: frontmatter, content } = matter(markdownWithMeta)
+  return {
+    props: {
+      frontmatter,
+      content,
+      slug,
+    },
+  }
+}
