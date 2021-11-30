626deb273f439408cbd74525bdb5c0e5b9ce6617
kriztiank <kriztiank@gmail.com>
2021-11-30 12:16:30 +0100

    Category Sidebar

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -3,19 +3,28 @@ import path from 'path'
 import matter from 'gray-matter'
 import Layout from '@/components/Layout'
 import Post from '@/components/Post'
+import CategoryList from '@/components/CategoryList'
 import { getPosts } from '@/lib/posts'
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>
-        Posts in {categoryName}
-      </h1>
+      <div className='flex justify-between flex-col md:flex-row'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            Posts in {categoryName}
+          </h1>
 
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   )
@@ -50,6 +59,12 @@ export async function getStaticProps({ params: { category_name } }) {
 
   const posts = getPosts()
 
+  // Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category)
+  // console.log(categories)
+  const uniqueCategories = [...new Set(categories)]
+  // console.log(uniqueCategories)
+
   // Filter posts by category
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
@@ -59,6 +74,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories
     },
   }
 }
